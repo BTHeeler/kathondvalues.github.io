31a321ddc3a808d447bf2a5d57bc8c31f0fa559b
BTHeeler <78782165+BTHeeler@users.noreply.github.com>
2021-12-22 03:13:21 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,119 +1,28 @@
 const ideologies = { // eslint-disable-line no-unused-vars
-  'Marxism-Leninism': {
+  'United Left Party (ULF)': {
+    a: 40, // Revolutionary vs. Reformist -- Practical Axis
+    b: 40, // Marxian vs. Revisionist -- Theoretical Axis
+    c: 30, // Statist vs. Anarchist -- Civic Axis
+    d: 50, // Furry vs. Normie -- Cultural Axis
+    e: 50, // Collective Ownership vs. State Ownership -- Economic Axis
+    f: 20, // Esoteric vs. Secular -- Religious Axis
+    g: 30, // Futurist vs. Moderate -- Progress Axis
+    h: 40, // Isolationist vs. Internationalist -- Diplomatic Axis
+    i: 40, // Ecology vs. Production -- Industrial Axis
+    j: 50, // Hyperspatial vs. Primitivist -- Acceleration Axis
+    k: 40  // Associationist vs. Vanguardist -- Organisational Axis
+  },
+  'Libertarian Communist Congress (LCC)': {
     a: 80,
-    b: 80,
-    c: 80,
-    d: 60,
-    e: 90,
-    f: 70,
-    g: 40
-  },
-  'Orthodox-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Eco-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 20,
-    g: 30
-  },
-  'Centrist-Marxism': {
-    a: 50,
-    b: 80,
-    c: 60,
-    d: 60,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Council-Communism': {
-    a: 100,
-    b: 70,
-    c: 40,
-    d: 80,
-    e: 50,
-    f: 50,
-    g: 30
-  },
-  'Left-Communism': {
-    a: 90,
-    b: 60,
-    c: 50,
-    d: 90,
-    e: 80,
-    f: 50,
-    g: 30
-  },
-  'Anarcho-Communism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 50,
-    g: 30
-  },
-  'Eco-Anarchism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 20,
-    g: 30
-  },
-  'Market-Anarchism': {
-    a: 20,
     b: 40,
-    c: 30,
-    d: 70,
-    e: 10,
-    f: 60,
-    g: 30
-  },
-  'Utopian-Socialism': {
-    a: 10,
-    b: 10,
-    c: 40,
-    d: 20,
-    e: 50,
-    f: 40,
-    g: 50
-  },
-  'Democratic-Socialism': {
-    a: 20,
-    b: 30,
-    c: 30,
-    d: 70,
-    e: 60,
+    c: 20,
+    d: 80,
+    e: 20,
     f: 30,
     g: 20
   },
-  'Social-Democracy': {
-    a: 10,
-    b: 30,
-    c: 50,
-    d: 40,
-    e: 80,
-    f: 50,
-    g: 30
-  },
-  'Left-Wing-Nationalism': {
-    a: 60,
-    b: 60,
-    c: 60,
-    d: 30,
-    e: 70,
-    f: 40,
-    g: 30
+  'ex': {
+    a: 0,
+    b: 0
   }
 }
